c3450e1444f6017c58b31510a6912509191f7d8d
nikesh <nikesh.strat@yahoo.com>
2017-08-23 15:27:07 +0100

    commentRemove

--- readXML_1.py
+++ readXML_1.py
@@ -10,31 +10,14 @@ def xmlParseBeforePublishing(xmlFile):
 
 		output: returns array of multidimensional matrix with all the information about the packets
 	'''
-	#xmlFile = sys.argv[1]
-	#print(xmlFile)
-	#xml parsing
 	tree = ET.parse(xmlFile)
 	#gets root which is <newSimulation>
 	root = tree.getroot()
 	#print (root)
-	#counts how many packets are there
-	packetCounter = 0
-	#packetContent = [[0 for i in range(100)]for j in range(50)]
-	#packets = x.find('packet')
 	packetContent = []
 	for child in root:
-		#packets =x.find('packet')
-		#print(child)
-		#print("packet counter:", packetCounter)
-		#subchildCounter = 0
-		#print(ET.tostring(child))
 		packetContent.append( ET.tostring(child))
 		#packetCounter = packetCounter + 1
 
 	return packetContent
-#x = etree.parse(xmlFile)
-#print(etree.tostring(xmlFile.child, pretty_print = True))
 
-#output = xmlParseBeforePublishing('SimulationXML/nikeshLama/Initialisation_file_nikeshLama1.xml')
-
-#print(output[0][0])
