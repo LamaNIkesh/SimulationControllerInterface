64a6a490841a71f260f1edb0716c6397f2a7d3c1
nikesh <nikesh.strat@yahoo.com>
2017-10-12 10:15:56 +0100

    docsAdd

---
+++ howtouse.php
@@ -0,0 +1,16 @@
+<?php
+include("head.html")
+?>
+
+<div class = "container">
+<div class="col-sm-12">
+<h2>How To Use</h2>
+
+<p>
+Please visit the online documentation to learn more about how to use the application. 
+<p><a href="https://webinterfacedocumentation.readthedocs.io" target="_blank">Documentation</p></a>
+<br><br>
+</div></div>
+<?php
+include("end_page.html")
+?>
