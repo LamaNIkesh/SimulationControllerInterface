6f5ab6c8daeadc748d74085b6d724a5bdc0778da
nikesh <nikesh.strat@yahoo.com>
2017-10-20 15:18:13 +0100

    tests

--- tcpSend/send_packet_tcp.py
+++ tcpSend/send_packet_tcp.py
@@ -4,10 +4,12 @@ import time
 import sys
 import numpy as np
 import readXML_1 as rx
+from termcolor import colored,cprint
+
 
 host = socket.gethostname()
-#host = 100.100.1.252 # host name is already configured in /etc/hosts ; IMserver has 100.100.1.252 IP address
-port = 60000
+host = "100.100.1.252" # host name is already configured in /etc/hosts ; IMserver has 100.100.1.252 IP address
+port = 3000
 packet = ""
 
 ##----sys.argv[1] will be used when calling this python script from php webpage directly.
@@ -15,12 +17,12 @@ packet = ""
 #xmlFile = sys.argv[1]
 xmlFile = '/home/nikesh/Documents/WebServer/SimulationControllerInterface/SimulationXML/nikeshLama/Initialisation_file_nikeshLama1.xml'
 #xmlFile = 'Initialisation_file_nikeshLama1.xml'
-
+#print_red_on_cyan = lambda x:cprint(x,'red','on_cyan')
 #function for tcp connection and packet transmission
 def TCPclient(host, port, packet):
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)    
-	print("+++++++++++Connection established with ", host, "port: ", port, "+++++++++++")	
-	print("Sending packet to ", host, port)
+	print colored(("+++++++++++Connection established with ", host,"port: ",port, "+++++++++++"),'cyan')	
+	print ("Sending packet to ", host, port)
 	try:
 		#TODO needs revision
 		sock.connect((host, port))
@@ -32,20 +34,25 @@ def TCPclient(host, port, packet):
 try:
 	#returns a list with all the packets 
 	MessageArray = rx.xmlParseBeforePublishing(xmlFile)
-	print("successfully read")
+		
+	print colored("successfully read",'green')
 #error handling
 except:
 	print("cannot read")
 
 #traverse through array elements and establish connection and send packet 
+'''
 for i in range(len(MessageArray)):
-	'''
-	loops through each packet and send them to the tcp server i.e. im server
-	'''
+	
+	#loops through each packet and send them to the tcp server i.e. im server
+	
 	packet= MessageArray[i]
-	print(packet) # just printing on the console
+	print colored(packet,'green') # just printing on the console
 	TCPclient(host,port,packet)
-
+'''
+packet = MessageArray[0]
+print colored(packet,'green')
+TCPclient(host,port,packet)
 	
 
 	
